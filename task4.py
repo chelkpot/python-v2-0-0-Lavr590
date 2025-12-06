e0316c74aca9b8aa3fde4f8630c9815b9f9380fe
Рассказов Роман <kartohahap12@gmail.com>
2025-12-06 20:17:07 +0500

    дз

---
+++ task4.py
@@ -0,0 +1,14 @@
+# tasks/task4.py
+
+def solve():
+# Ниже пишите решение задачи
+    
+    phrase1 = input()
+    phrase2 = input()
+    phrase3 = input()
+    print(phrase1, phrase2, phrase3, sep='---')
+    
+
+# Код ниже не трогать! он нужен для тестов
+if __name__ == "__main__":
+    solve()
---
+++ task5.py
@@ -0,0 +1,11 @@
+# tasks/task5.py
+
+def solve():
+# Ниже пишите решение задачи
+
+    spell = input()
+    print(spell, end=' - Сказала Гермиона!\n')
+   
+# Код ниже не трогать! он нужен для тестов
+if __name__ == "__main__":
+    solve()
